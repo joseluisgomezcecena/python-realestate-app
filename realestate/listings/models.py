bf1cae0cb09a0f6847ee4ced0dbbfda52bdffdac
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-01-24 23:42:43 -0800

    added modules

--- realestate/listings/models.py
+++ realestate/listings/models.py
@@ -15,7 +15,7 @@ class Listing(models.Model):
     bedrooms = models.IntegerField()    
     bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
     garage = models.IntegerField(default=0)    
-    sqft = models.IntegerChoices()
+    sqft = models.IntegerField()
     lotsize = models.DecimalField(max_digits=6, decimal_places=2)    
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d')
     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -119,3 +119,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realestate/static')
     ]
+
+#media folder
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')    
+MEDIA_URL = '/media/'
