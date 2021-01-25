06b09bf7c31adf5f8174d0a7f4a57f0d3e65ffa5
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-01-25 14:14:51 -0800

    display listings on front end template

--- realestate/listings/admin.py
+++ realestate/listings/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Listing
+from django.utils.html import format_html
 
-admin.site.register(Listing)
+class listingAdmin(admin.ModelAdmin):
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" />'.format(obj.image.url))
+    
+    image_tag.short_description = 'photo_main'
+
+
+
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor', 'photo_main')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor', 'zip_code', 'address', 'city', 'state')
+    list_per_page = 25
+
+admin.site.register(Listing, listingAdmin)
 
 # Register your models here.
