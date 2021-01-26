1cd3a83afb18e39aeb997bcaa765ad72be87b9d3
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-01-25 16:56:49 -0800

    finished displaying dynamic data list

--- realestate/pages/views.py
+++ realestate/pages/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return render(request, 'pages/index.html')
+from listings.models import Listing
+from realtors.models import Realtor
 
+# index view.
+def index(request):
+    lisings = Listing.objects.order_by('-list_date').filter(is_published = True)[:3]
+    context = {
+        'listings': lisings
+    }
+    return render(request, 'pages/index.html', context)
+
+
+
+
+# about view.
 def about(request):
-    return render(request, 'pages/about.html')    
+
+    #get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    #get mvp
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)    
