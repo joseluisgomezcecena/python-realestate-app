456ec0ad0d09643a2f0e5f789a30ca204892c2ed
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-01-25 16:11:06 -0800

    finished pagination

--- realestate/listings/views.py
+++ realestate/listings/views.py
@@ -6,7 +6,8 @@ from .models import Listing
 # Create your views here.
 def index(request):
 
-    listings = Listing.objects.all()
+    #listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
     paginator = Paginator(listings, 2)
     page = request.GET.get('page')
